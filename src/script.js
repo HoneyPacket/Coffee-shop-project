164d84c83b251ea7833f5b8b1164f9e9fc826ebb
HoneyPacket <Brutledge311@gmail.com>
2023-02-10 20:06:04 -0500

    Modified navbar

--- src/script.js
+++ src/script.js
@@ -1,7 +1,37 @@
 //reactive navbar code
-const hamToggle = document.getElementsByClassName('hamburgerMenu')[0]
-const navbarLinks = document.getElementsByClassName('navbarLinks')[0]
 
-hamToggle.addEventListener('click', ()=>{
-    navbarLinks.classList.toggle('active')
-})
+let toggleNavStatus = false;
+
+
+let toggleNav = function(){
+    let getSidebar = document.querySelector('.nav-sidebar');
+    let getSidebarUl = document.querySelector('.nav-sidebar ul');
+    let getSidebarA = document.querySelectorAll('.nav-sidebar a');
+
+    if (toggleNavStatus === false) {
+        getSidebarUl.style.visibility = "visible";
+        getSidebar.style.width = "272px";
+        getSidebar.style.visibility = "visible";
+
+
+        let arrayLength = getSidebarA.length;
+        for (let i = 0; i < arrayLength; i++) {
+            getSidebarA[i].style.opacity = '1';
+        }
+
+        toggleNavStatus = true;
+    }
+    else if (toggleNavStatus === true) {
+        getSidebarUl.style.visibility = "hidden";
+        getSidebar.style.width = "1px";
+        getSidebar.style.visibility = "hidden";
+
+        let arrayLength = getSidebarA.length;
+        for (let i = 0; i < arrayLength; i++) {
+            getSidebarA[i].style.opacity = '0';
+        }
+
+        toggleNavStatus = false;
+    }
+}
+
